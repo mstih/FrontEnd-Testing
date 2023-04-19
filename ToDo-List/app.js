29333b1ebfd0c956303263be928c639af2b8e5de
mstih <mstih@proton.me>
2023-04-19 12:58:42 +0200

    small fixes/upgrades
    flex display for items fixed,
    remove icon added,
    adding tasks by pressing enter button now supported.

--- ToDo-List/app.js
+++ ToDo-List/app.js
@@ -1,21 +1,37 @@
-let numOfTasks = 0;
+var numOfTasks = 0;
+var addButton = document.querySelector('button');
+var inputField = document.getElementById('input-task');
+
+//support for enter button instead of add button
+inputField.addEventListener("keypress", function(event){
+    if(event.key == "Enter"){
+        event.preventDefault();
+        addButton.click();
+    };
+});
 
 function addToList(){
     let content = document.getElementById('content');
     let text = document.getElementById('input-task').value;
     console.log(text);
-    if (text !== ""){
-        let newTask = document.createElement('div');
+    //checks if empty --> not added to the list
+    if (text !== ""){   
+        let newTask = document.createElement("div");
+        newTask.className = 'item';
+        newTask.id = "task" + numOfTasks.toString;
         newTask.innerHTML = `<i class="icon-check-empty"></i>
-                                <h4>` +text+ '</h4>';
+                                <h4>` +text+ `</h4>
+                                <i class="icon-remove"></i>`;
         content.appendChild(newTask);
         numOfTasks++;
-    }else{
-        let placeholder = document.getElementById('input-task');
     };
 };
 
 
 function removeFromList(){
+    //for clicking cross and remove item -- later added
+}
 
+function toogleDone(){
+    //for clicking empty square and changing it to task completed
 }

a43a507f5aa0735fee682a1b141de4fc8f50367b
mstih <mstih@proton.me>
2023-04-20 17:25:36 +0200

    it works
    adding tasks, removing tasks and marking tasks as completed is working, however I was thinking about having it to store data locally to retain the state as it was when you closed the website

--- ToDo-List/app.js
+++ ToDo-List/app.js
@@ -1,6 +1,7 @@
 let numOfTasks = 0;
 let content = document.getElementById('content');
 let addButton = document.getElementById('add-btn');
+let warning = document.getElementById('warning');
 //support for enter button instead of add button
 document.getElementById('task-input').addEventListener("keypress", function(event){
     if(event.key == "Enter"){
@@ -9,30 +10,34 @@ document.getElementById('task-input').addEventListener("keypress", function(even
     };
 });
 
-//adds a new task from inputed text (if empty => does nothing)
+//function which adds a new task from inputed text (if empty => does nothing)
 function addToList(){
     let text = document.getElementById('task-input');
     if(text.value != ""){
+        warning.style.display = "none";
         let newTask = document.createElement("div");
         newTask.className = 'item';
         newTask.id = "task" + numOfTasks.toString();
-        newTask.innerHTML = `<i class="icon-check-empty" onclick="toogleDone(${newTask.id})"></i>
+        newTask.innerHTML = `<i class="icon-check-empty" onclick="toogleDone('${newTask.id}')"></i>
                                 <h4>` +text.value+ `</h4>
-                                <i class="icon-remove" onclick="removeFromList(${newTask.id})"></i>`;
+                                <i class="icon-remove" onclick="removeFromList('${newTask.id}')"></i>`;
         content.appendChild(newTask);
         numOfTasks++;
         text.value = "";
+    }else{
+        warning.innerHTML = "You need to enter a task name"
+        warning.style.display = "block";
     }   
 };
 
-//function which removes selected element from the list --NOT WORKING YET--
+//function which removes selected element from the list
 function removeFromList(taskID){
     let taskToRemove = document.getElementById(taskID);
     content.removeChild(taskToRemove);
     console.log("Removed item with id:" + taskID);
 }
 
-//function which changes the icon to done --NOT WORKING YET--
+//function which changes the icon to done
 function toogleDone(taskID){
     let oldHtml = document.getElementById(taskID);
     let temp = oldHtml.innerHTML.toString();
